6abffb669e602ec759e963a005de86e251bf7e48
arrowmin <botbot_aromin@yahoo.com>
2024-10-10 21:48:43 -0700

    4.2.3 Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,97 @@
 import "./App.css";
 import { Label, Note } from "./types"; // Import the Label type from the appropriate module
 import { dummyNotesList } from "./constants"; // Import the dummyNotesList from the appropriate module
+//import ToggleTheme from "./hooksExercise";
+import { useContext, useEffect, useState } from "react";
+import { ThemeContext, themes } from "./ThemeContext";
 
 function App() {
+  const [currentTheme, setCurrentTheme] = useState(themes.light);
+
+  const toggleTheme = () => {
+    setCurrentTheme(currentTheme === themes.light ? themes.dark : themes.light);
+  };
+
+  useEffect(() => {
+    document.body.style.background = currentTheme.background;
+    document.body.style.color = currentTheme.foreground;
+  }, [currentTheme]);
+
   return (
-    <div className="app-container">
-      <form className="note-form">
-        <div>
-          <input placeholder="Note Title"></input>
-        </div>
+    <ThemeContext.Provider value={currentTheme}>
+      <button
+        style={{
+          background: currentTheme.background,
+          color: currentTheme.foreground,
+        }}
+        onClick={toggleTheme}
+      >
+        {" "}
+        Toggle Theme{" "}
+      </button>
 
-        <div>
-          <textarea></textarea>
-        </div>
-
-        <div>
-          <button type="submit">Create Note</button>
-        </div>
-      </form>
-
-      <div className="notes-grid">
-        {dummyNotesList.map((note) => (
-          <div key={note.id} className="note-item">
-            <div className="notes-header">
-              <button>x</button>
-            </div>
-            <h2> {note.title} </h2>
-            <p> {note.content} </p>
-            <p> {note.label} </p>
+      <div
+        style={{
+          background: currentTheme.background,
+          color: currentTheme.foreground,
+        }}
+        className="app-container"
+      >
+        <form className="note-form">
+          <div>
+            <input
+              placeholder="Note Title"
+              style={{
+                background: currentTheme.background,
+                color: currentTheme.foreground,
+                borderColor: currentTheme.foreground,
+              }}
+            ></input>
           </div>
-        ))}
+
+          <div>
+            <textarea
+              style={{
+                background: currentTheme.background,
+                color: currentTheme.foreground,
+                borderColor: currentTheme.foreground,
+              }}
+            ></textarea>
+          </div>
+
+          <div>
+            <button type="submit">Create Note</button>
+          </div>
+        </form>
+        <div className="notes-grid">
+          {dummyNotesList.map((note) => (
+            <div
+              key={note.id}
+              style={{
+                background: currentTheme.background,
+                color: currentTheme.foreground,
+              }}
+              className="note-item"
+            >
+              <div className="notes-header">
+                <button
+                  style={{
+                    background: currentTheme.background,
+                    color: currentTheme.foreground,
+                  }}
+                >
+                  x
+                </button>
+              </div>
+              <h2> {note.title} </h2>
+              <p> {note.content} </p>
+              <p> {note.label} </p>
+            </div>
+          ))}
+        </div>
+        \{" "}
       </div>
-    </div>
+    </ThemeContext.Provider>
   );
 }
 
